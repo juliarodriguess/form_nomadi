ffc0c0960fc3f64b305285a964a84fa9289a7a2f
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-11-01 16:05:25 -0300

    Incluir browser router e path para páginas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import Origem from './pages/Origem/Origem';
 import Destino from './pages/Destino/Destino'
 import PortageDate from './pages/PortageDate/PortageDate'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
-const divForm = document.getElementById('form-pedido')
+function App() {
+    return (
+        <div className='app'>
+        {/* NavBar*/}
+        <Switch>
+            <Route path='/' exact component={Origem} />
+            <Route path='/destino' component={Destino} />
+            <Route path='' component={Origem} />
+        </Switch>
+        </div>
+    )
+}
 
 
-ReactDOM.render(<PortageDate />, divForm)
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('form-pedido'))
