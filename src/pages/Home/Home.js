d34b5943ddb90e82059389b05daeda2cb660073a
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-11-12 16:59:39 -0200

    Ajustar responsividade

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Home.css'
-import 'bootstrap/dist/css/bootstrap.css'
 import ButtonNext from '../../components/ButtonNext/ButtonNext';
 
 function Home () {
     return (
         <main className="home">
-            <h1>Tão simples que nem parece mudança.</h1>
-            <section>
+            <section className="title-home">
+                <h1>Tão simples que nem parece mudança.</h1>
+            </section>
+            <section className="buttons-home">
                 <ButtonNext url="/pedido">Fazer um pedido</ButtonNext>
                 <ButtonNext url="/pedido">Falar com um consultor</ButtonNext> 
                 {/* Abrir modal com informações básicas */}
