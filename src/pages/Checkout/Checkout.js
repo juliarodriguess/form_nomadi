a45e6b2567d00cba106e4d1403c6f558a41049c8
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-12-26 12:58:12 -0200

    Mostrar dados na página de contato

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import checkoutServices from '../../services/checkout'
+import axios from 'axios'
 import SampleBtn from '../../components/SampleBtn/SampleBtn'
 import ButtonNext from '../../components/ButtonNext/ButtonNext'
 import SampleModal from '../../components/SampleModal/SampleModal'
@@ -11,7 +11,8 @@ class Checkout extends Component {
         super(props)
         this.state = {
             open: false,
-            data:{}
+            data:{},
+            loaded: false
         }
     }
 
@@ -25,43 +26,42 @@ class Checkout extends Component {
         this.setState({ open: !this.state.open })
     }
 
-    // async dataToLoad () {
-    //     console.log("1ª",this.state.data)
-    //     const dataToLoad = await checkoutServices.getDataPortage().then((response) => {
-    //         return response.data[0]
-    //     })
-    //     console.log(dataToLoad)
-    //     this.setState({data: dataToLoad})
-    //     console.log("2ª",this.state.data.origin.address)
-    // }
-
-    // async componentWillMount() {
-    //     const data = await this.dataToLoad()
-    //     return data
-    // }
+    componentWillMount() {
+        axios({
+            method:'get',
+            url:'https://immense-dawn-93107.herokuapp.com/api/portage/'
+        })
+        .then ((response) => {
+            const data = response.data[0]
+            this.setState({data: data, loaded: true})
+            console.log("then", this.state.data)
+        })
+    }
     
 
     render() {
         console.log("3ª", this.state.data)
         return(
             <main className="checkout">
+            {this.state.loaded && 
+                <>
                 <h1>Resumo do seu pedido</h1>
                 <section className="address-checkout">
                     <div className="address-checkout__input">
                         <img  className="address-checkout__img" src="https://image.flaticon.com/icons/svg/326/326660.svg"/>
-                        {/* <p><strong>Endereço de retirada:</strong> 
+                        <p><strong>Endereço de retirada: </strong> 
                         {this.state.data.origin.address}
-                        </p> */}
+                        </p>
                     </div>
                     <div className="address-checkout__input">
                     <img  className="address-checkout__img" src="https://image.flaticon.com/icons/svg/326/326660.svg"/>
                     <p>
-                    <strong>Endereço de entrega:</strong> Alameda Eduardo Prado, 150. Campos Elíseos. São Paulo - SP.</p>
+                    <strong>Endereço de entrega:</strong> {this.state.data.destination.address}</p>
                     </div>
                 </section>
                 <section className="select-items-checkout">
                     <img  className="select-items-checkout__img" src="https://image.flaticon.com/icons/svg/1044/1044288.svg"/>
-                    <p><strong>Volume do frete: </strong>20 itens.</p>
+                    <p><strong>Volume do frete: </strong>{this.state.data.listOfItems.length}</p>
                 </section>
                 <section className="date-checkout">
                     <img  className="date-checkout__img" src="https://image.flaticon.com/icons/svg/942/942759.svg"/>
@@ -69,14 +69,16 @@ class Checkout extends Component {
                 </section>
                 <section className="time-checkout">
                     <img  className="time-checkout__img" src="https://image.flaticon.com/icons/svg/1303/1303672.svg"/>
-                    <p><strong>Período preferido: </strong>De manhã (de 09h às 12h).</p>
+                    <p><strong>Período preferido: </strong>{this.state.data.period.hour}</p>
                 </section>
                 <h5 className="checkout-text">E aí, tudo ok?</h5>
                     <SampleBtn className="btn_checkout__btn" onClick={this.showModal}>Sim, pedir!</SampleBtn>
                     <ButtonNext url="/" className="btn_checkout__btn">Não, quero cancelar.</ButtonNext>
                     {this.state.open && <SampleModal onClickClose={this.hideModal}><h3>Seu pedido foi recebido!</h3></SampleModal>}
+            </>
+            }
             </main>
-        )
+            )
     }
 }
 
